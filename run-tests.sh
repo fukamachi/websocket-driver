ba835b7c41744abee5e8e7910d39c311d86daccb
Eitaro Fukamachi <e.arrows@gmail.com>
2017-02-11 21:44:36 +0900

    Call clack-socket:start-connection at the end of start-connection.

--- run-tests.sh
+++ run-tests.sh
@@ -3,9 +3,15 @@
 ros -l websocket-driver-server.asd -l examples/echo-server.lisp &
 SERVER_PID=$!
 
+count=0
 while true; do
     nc -z 127.0.0.1 5000 >/dev/null 2>&1 && break
     sleep 1
+    count=$(( count + 1 ))
+    if [ 60 -lt $count ]; then
+        echo "Too long time took to start a server."
+        exit 1;
+    fi
 done
 
 RESULT=$(node t/client.js)
